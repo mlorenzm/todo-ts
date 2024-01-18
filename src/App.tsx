5bc8ca4a566ce8b6ef4b870dd6803e85e2655d5a
mlorenzm <mlorenzomohamed@gamil.com>
2024-01-18 16:16:29 +0100

    add daisyUI and Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
 
   return (
     <>
-      <div>
+      <div className='bg-lime-400'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react bg-lime-400" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="bg-white text-red-500">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
